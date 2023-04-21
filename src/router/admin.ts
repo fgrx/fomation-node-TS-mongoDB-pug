9c51d103b4b9a2d772f027e5dcb98bf4ca5062c4
Fabien Grignoux <fabien.grignoux@outlook.com>
2023-04-21 17:04:26 +0200

    Ajout du route guard sur l'admin

--- src/router/admin.ts
+++ src/router/admin.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { hikeManagement } from "../controllers/admin/hikeManagement";
+import { routeGuard } from "../middlewares/routeGuard";
 
 const adminRoutes = (router: Router): Router => {
-  router.get("/admin", hikeManagement.displayHikeManagementConsole);
-  router.get("/admin/delete/:slug", hikeManagement.hikeDeletion);
-  router.get("/admin/valid/:slug", hikeManagement.hikeValidation);
+  router.get("/admin", routeGuard, hikeManagement.displayHikeManagementConsole);
+  router.get("/admin/delete/:slug", routeGuard, hikeManagement.hikeDeletion);
+  router.get("/admin/valid/:slug", routeGuard, hikeManagement.hikeValidation);
 
   return router;
 };
