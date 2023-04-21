b691884e2f081fb698b3647a8e12d2fe990895ec
Fabien Grignoux <fabien.grignoux@outlook.com>
2023-04-21 09:42:14 +0200

    Message de succès ou d'échec

--- src/controllers/hikeAddController.ts
+++ src/controllers/hikeAddController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { Hike } from "../interfaces/hike";
 import slugify from "slugify";
 import { hikeRepository } from "../repositories/hikeRepository";
+import { AlertMessage } from "../interfaces/AlertMessage";
 
 const hikeAddController = {
   displayForm: (req: Request, res: Response) => {
@@ -46,7 +47,21 @@ const hikeAddController = {
     };
 
     const createdHike = await hikeRepository.addHike(newHike);
-    console.log(createdHike);
+
+    const message: AlertMessage = {
+      title: "Randonnée enregistrée !",
+      description: "Votre randonnée a été ajoutée avec succès",
+      type: "success",
+    };
+
+    if (!createdHike) {
+      message.title = "Une erreur s'est produite";
+      message.description =
+        "Une erreur s'est produite pendant l'enregistrement. Veuillez contacter l'administrateur.";
+      message.type = "error";
+    }
+
+    res.render("hikeAddForm", { message, defaultHike: newHike });
   },
 };
 
