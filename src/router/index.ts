e3de90d83ae9658a34c93e16154ac177ce868d7a
Fabien Grignoux <fabien.grignoux@outlook.com>
2023-04-21 11:50:24 +0200

    Formulaire de connexion

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,14 @@ import { homeController } from "../controllers/homeController";
 import { useRouteError } from "../middlewares/routeError";
 import { hikeRoutes } from "./hike";
 import { adminRoutes } from "./admin";
+import { authController } from "../controllers/authController";
 
 const createRouter = (app: Application) => {
   const router = Router();
 
   router.get("/", homeController);
   router.get("/mentions-legales", legalController);
+  router.get("/login", authController.showLogin);
   app.use(router);
 
   const hikeRouter = hikeRoutes(router);
